04ac75461775e83c725851166d969919dca2de5b
sumit <sumitvirdi50@gmail.com>
2025-01-30 16:23:04 +0530

    enhancements

--- service.js
+++ service.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const secret = process.env.SECRET_KEY;
 
 const setToken = (user) => {
-    return jwt.sign(user, secret);
+    return jwt.sign(user, secret, {expiresIn:"1h"});
 }
 
 module.exports = { setToken }
